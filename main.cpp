7cf36dd8575aefaf6263c00ce2d17a2d97ea6161
andigor <amotsok@gmail.com>
2016-03-06 14:28:30 +0200

    xadd mt

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <mutex>
 
 
+std::mutex mut;
 
 sqlite3_stmt* prepare_query(sqlite3 * db, const char * const sql)
 {
@@ -39,13 +40,15 @@ void reset_query(sqlite3 * db, sqlite3_stmt * prepared)
 std::vector<std::string> read_data(sqlite3 * db, sqlite3_stmt * prepared)
 {
   std::vector<std::string> ret;
-  reset_query(db, prepared);
-
-  int rc = sqlite3_step(prepared);
-
-  while (rc == SQLITE_ROW) {
-    ret.push_back( (const char *)sqlite3_column_text(prepared, 1) ); 
+//  reset_query(db, prepared);
+  int rc;
+  while (true) {
+    mut.lock();
     rc = sqlite3_step(prepared);
+    if (rc != SQLITE_ROW)
+      break;
+    ret.push_back( (const char *)sqlite3_column_text(prepared, 1) ); 
+    mut.unlock();
   }
 
   if (rc != SQLITE_DONE) {
@@ -87,9 +90,26 @@ void process()
   sqlite3_close(db_);
 }
 
+void process_mt()
+{
+  sqlite3* db_ = open_database("test1.db");
+  sqlite3_stmt * stmt_ = prepare_query(db_, "select * from company");
+
+
+  std::thread thr1(process_data, db_, stmt_);
+  std::thread thr2(process_data, db_, stmt_);
+
+  thr1.join();
+  thr2.join();
+
+  sqlite3_finalize(stmt_);
+
+  sqlite3_close(db_);
+}
+
 int main()
 {
-  process();
+  process_mt();
   return 0;
 }
 
